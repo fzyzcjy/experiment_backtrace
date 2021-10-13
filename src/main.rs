a0872d965d0fc414803c6abe562496be674301b0
fzyzcjy <ch271828n@outlook.com>
2021-10-13 17:49:53 +0800

    more

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,18 @@ use anyhow::{anyhow, Result};
 fn main() {
     println!("main start");
     let err = simple_recursive_function().err().unwrap();
-    println!("err={:?}", err);
+    println!("The error object I see: {:?}", err);
     println!("main end");
 }
 
 pub fn simple_recursive_function() -> Result<()> {
-    if rand::random::<f32>() < 0.8 {
+    if rand::random::<f32>() < 0.9 {
         simple_recursive_function()?;
         simple_recursive_function()?;
         Ok(())
     } else {
         // print the backtrace
+        println!("------------print backtrace------------");
         backtrace::trace(|frame| {
             println!("backtrace::trace frame={:?}", frame);
             backtrace::resolve_frame(frame, |symbol| {
@@ -21,6 +22,7 @@ pub fn simple_recursive_function() -> Result<()> {
             });
             true // keep going to the next frame
         });
+        println!("---------------------------------------");
 
         // return anyhow error
         Err(anyhow!("deliberate error!"))
