4da22c72bb46fe9a28191c19be6392a15e22cd94
fzyzcjy <ch271828n@outlook.com>
2021-10-13 17:59:18 +0800

    add release mode

--- run_android.sh
+++ run_android.sh
@@ -4,3 +4,8 @@ echo "run debug"
 cargo ndk --target armeabi-v7a build
 adb push target/armv7-linux-androideabi/debug/experiment_backtrace /data/local/tmp/experiment_backtrace
 adb shell 'chmod +x /data/local/tmp/experiment_backtrace && /data/local/tmp/experiment_backtrace'
+
+echo "run release"
+cargo ndk --target armeabi-v7a build --release
+adb push target/armv7-linux-androideabi/release/experiment_backtrace /data/local/tmp/experiment_backtrace
+adb shell 'chmod +x /data/local/tmp/experiment_backtrace && /data/local/tmp/experiment_backtrace'
